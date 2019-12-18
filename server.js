87a1a2579e77fe557b8971b9ebc6b964c60b47a3
Jennifer Powell <jerpowel321@gmail.com>
2019-12-17 17:49:52 -0800

    add route for scraping news articles and saving each article db

--- server.js
+++ server.js
@@ -20,6 +20,37 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
+// ROUTES
+// A GET route for scraping the echoJS website
+app.get("/scrape", function(req,res){
+    axios.get("http://www.echojs.com/")
+    .then(function(response){
+        var $ = cheerio.load(response.data);
+        
+        $("article h2").each(function(i,elecment) {
+// Save an empty result object
+
+            var result = {}; 
+// Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this)         .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+// Create a new Article using the `result` object built from scraping
+            db.Article.create(result).then(function(dbArticle){
+// View the added result in the console
+                console.log(dbArticle);
+            })
+            .catch(function(err){
+                console.log(err);
+            });
+        })
+        res.send("Scrape Complete")
+    });
+});
+
+
 
 
 app.listen(PORT, function() {
