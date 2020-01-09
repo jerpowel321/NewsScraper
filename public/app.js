60459e064e13f719aab7340773a0a0ef274e8446
Jennifer Powell <jerpowel321@gmail.com>
2020-01-08 20:51:11 -0800

    update save button for individual articles

--- public/app.js
+++ public/app.js
@@ -32,17 +32,19 @@ $.getJSON("/articles", function(data) {
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
       $("#articlesContainer").append(
-          "<div class='card' style='width: 20rem; <div class='card-body'><h5 class='card-title' data-id='" + data[i]._id + "'>" + data[i].title + "</h5><p class='card-text'>"+ data[i].description + "</p><div><a class='mr-2 btn btn-outline-primary my-2 my-sm-0' target='_blank' href=https://www.nytimes.com"  + data[i].link + "> Link to Article </a><button class='saveArticle-button btn btn-md btn-primary text-white p-1'>Save Article</button></div></div></div>");
+          "<div class='card' style='width: 20rem; <div class='card-body'><h5 class='card-title'>" + data[i].title + "</h5><p class='card-text'>"+ data[i].description + "</p><div><a class='mr-2 btn btn-outline-primary my-2 my-sm-0' target='_blank' href=https://www.nytimes.com"  + data[i].link + "> Link to Article </a><button class='save-button btn btn-md btn-primary text-white p-1' type='submit' data-id='" + data[i]._id + "'>Save Article</button></div></div></div>");
     }
   });
 
 // event listener on the "save" buttons
-$(document).on("click", ".saveArticle-button", function(event) {
-    // prevent the page from refreshing
+$(document).on("click", ".save-button", function(event) {
+    // prevent the page to refresh
     event.preventDefault();
+
     // grab the id of the article whose button has been clicked
     var articleID = $(this).data("id");
     console.log(articleID);
+
     // post request to update the value of "save" in the Article collection
     $.ajax("/save-article/" + articleID, {
         method: "POST"
@@ -51,3 +53,4 @@ $(document).on("click", ".saveArticle-button", function(event) {
         location.reload();
     });
 });
+
